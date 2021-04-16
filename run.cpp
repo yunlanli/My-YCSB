e21df6ed6675b7cd283cc94de7593abc10c48480
Yuhong Zhong <nanua.zqz@gmail.com>
2021-04-16 16:17:40 -0400

    add warm-up

--- run.cpp
+++ run.cpp
@@ -47,16 +47,6 @@ void monitor_thread_fn(const char *task, OpMeasurement *measurement) {
 	       measurement->get_throughput(GET) + measurement->get_throughput(SET));
 }
 
-enum {
-	PARAM_KEY_SIZE = 1,
-	PARAM_VALUE_SIZE,
-	PARAM_NR_ENTRY,
-	PARAM_NR_THREAD,
-	PARAM_READ_RATIO,
-	PARAM_NR_OP,
-	PARAM_ARGC
-};
-
 void initialize_workload(WiredTigerFactory *factory, long nr_entry, long key_size, long value_size, unsigned int seed) {
 	InitWorkload init_workload(nr_entry, key_size, value_size, 'i');
 	OpMeasurement init_measurement;
@@ -72,9 +62,52 @@ void initialize_workload(WiredTigerFactory *factory, long nr_entry, long key_siz
 	factory->destroy_client(init_client);
 }
 
+void run_workload(const char *task, WiredTigerFactory *factory, long nr_entry, long key_size, long value_size, int nr_thread, double read_ratio, long nr_op) {
+	/* allocate resources */
+	Client **client_arr = new Client *[nr_thread];
+	RandomWorkload **workload_arr = new RandomWorkload *[nr_thread];
+	std::thread **thread_arr = new std::thread *[nr_thread];
+	OpMeasurement measurement;
+	for (int thread_index = 0; thread_index < nr_thread; ++thread_index) {
+		client_arr[thread_index] = factory->create_client();
+		workload_arr[thread_index] = new RandomWorkload(key_size, value_size, nr_entry, nr_op, read_ratio, thread_index);
+	}
+
+	/* start running workload */
+	measurement.start_measure();
+	measurement.set_max_progress(nr_op * nr_thread);
+	for (int thread_index = 0; thread_index < nr_thread; ++thread_index) {
+		thread_arr[thread_index] = new std::thread(worker_thread_fn, client_arr[thread_index], workload_arr[thread_index], &measurement);
+	}
+	std::thread stat_thread(monitor_thread_fn, task, &measurement);
+	for (int thread_index = 0; thread_index < nr_thread; ++thread_index) {
+		thread_arr[thread_index]->join();
+	}
+	measurement.finish_measure();
+	stat_thread.join();
+
+	/* cleanup */
+	for (int thread_index = 0; thread_index < nr_thread; ++thread_index) {
+		factory->destroy_client(client_arr[thread_index]);
+		delete workload_arr[thread_index];
+		delete thread_arr[thread_index];
+	}
+}
+
+enum {
+	PARAM_KEY_SIZE = 1,
+	PARAM_VALUE_SIZE,
+	PARAM_NR_ENTRY,
+	PARAM_NR_THREAD,
+	PARAM_READ_RATIO,
+	PARAM_WARM_UP_OP,
+	PARAM_NR_OP,
+	PARAM_ARGC
+};
+
 int main(int argc, char *argv[]) {
 	if (argc != PARAM_ARGC) {
-		printf("Usage: %s <key size> <value size> <number of entries> <number of threads> <read ratio> <number of ops>\n", argv[0]);
+		printf("Usage: %s <key size> <value size> <number of entries> <number of threads> <read ratio> <number of warm-up ops> <number of ops>\n", argv[0]);
 		return EINVAL;
 	}
 	long key_size = atol(argv[PARAM_KEY_SIZE]);
@@ -82,6 +115,7 @@ int main(int argc, char *argv[]) {
 	long nr_entry = atol(argv[PARAM_NR_ENTRY]);
 	int nr_thread = atol(argv[PARAM_NR_THREAD]);
 	double read_ratio = atof(argv[PARAM_READ_RATIO]);
+	long nr_warm_up_op = atol(argv[PARAM_WARM_UP_OP]);
 	long nr_op = atol(argv[PARAM_NR_OP]);
 
 	WiredTigerFactory factory(nullptr, nullptr, nullptr, nullptr, nullptr, true, nullptr);
@@ -90,23 +124,6 @@ int main(int argc, char *argv[]) {
 	initialize_workload(&factory, nr_entry, key_size, value_size, 'i');
 
 	factory.update_cursor_config(nullptr);
-	Client **client_arr = new Client *[nr_thread];
-	RandomWorkload **workload_arr = new RandomWorkload *[nr_thread];
-	std::thread **thread_arr = new std::thread *[nr_thread];
-	OpMeasurement measurement;
-	for (int thread_index = 0; thread_index < nr_thread; ++thread_index) {
-		client_arr[thread_index] = factory.create_client();
-		workload_arr[thread_index] = new RandomWorkload(key_size, value_size, nr_entry, nr_op, read_ratio, thread_index);
-	}
-	measurement.start_measure();
-	measurement.set_max_progress(nr_op * nr_thread);
-	for (int thread_index = 0; thread_index < nr_thread; ++thread_index) {
-		thread_arr[thread_index] = new std::thread(worker_thread_fn, client_arr[thread_index], workload_arr[thread_index], &measurement);
-	}
-	std::thread stat_thread(monitor_thread_fn, "RandomWorkload", &measurement);
-	for (int thread_index = 0; thread_index < nr_thread; ++thread_index) {
-		thread_arr[thread_index]->join();
-	}
-	measurement.finish_measure();
-	stat_thread.join();
+	run_workload("Warm-Up", &factory, nr_entry, key_size, value_size, nr_thread, read_ratio, nr_warm_up_op);
+	run_workload("Random-Workload", &factory, nr_entry, key_size, value_size, nr_thread, read_ratio, nr_op);
 }
