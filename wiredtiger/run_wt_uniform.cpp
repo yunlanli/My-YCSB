0c5142717291b32078fb1fb434ea93c6a25f51d3
Yuhong Zhong <nanua.zqz@gmail.com>
2021-06-04 23:28:05 -0700

    minor fix

--- wiredtiger/run_wt_uniform.cpp
+++ wiredtiger/run_wt_uniform.cpp
@@ -26,7 +26,7 @@ int main(int argc, char *argv[]) {
 	long nr_warm_up_op = atol(argv[PARAM_WARM_UP_OP]);
 	long nr_op = atol(argv[PARAM_NR_OP]);
 
-	WiredTigerFactory factory(nullptr, nullptr, nullptr, nullptr, nullptr, false, nullptr);
+	WiredTigerFactory factory(nullptr, nullptr, nullptr, nullptr, nullptr, false, nullptr, false);
 
 	factory.update_cursor_config(nullptr);
 	if (nr_warm_up_op != 0) {
