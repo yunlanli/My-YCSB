14068cb645989b3ddf963d53d68f035c5a5271b3
Yuhong Zhong <nanua.zqz@gmail.com>
2021-06-18 11:21:31 -0700

    Update run_redis.sh

--- script/run_redis.sh
+++ script/run_redis.sh
@@ -16,8 +16,7 @@ cd $WORK_DIR/redis
 sudo sysctl vm.overcommit_memory=1
 sudo bash -c "echo never > /sys/kernel/mm/transparent_hugepage/enabled"
 sudo bash -c "echo never > /sys/kernel/mm/transparent_hugepage/defrag"
-./src/redis-server ./redis.conf
 for i in $(seq 1 $1)
 do
-    "./src/redis-server ./redis.conf --port $((6379 + $i))" &
+    ./src/redis-server ./redis.conf --port $((6379 + $i)) &
 done
