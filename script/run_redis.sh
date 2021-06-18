181f5bfa1ef8d85a549b34b095b7b337ad48f9e9
Yuhong Zhong <nanua.zqz@gmail.com>
2021-06-18 11:13:09 -0700

    Create run_redis.sh

---
+++ script/run_redis.sh
@@ -0,0 +1,23 @@
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <number of redis servers>"
+    return 1
+fi
+
+WORK_DIR=$(pwd)
+
+# stop redis
+while [ -n "$(pgrep redis)" ]
+do
+    kill $(pgrep redis)
+done
+
+# run redis
+cd $WORK_DIR/redis
+sudo sysctl vm.overcommit_memory=1
+sudo bash -c "echo never > /sys/kernel/mm/transparent_hugepage/enabled"
+sudo bash -c "echo never > /sys/kernel/mm/transparent_hugepage/defrag"
+./src/redis-server ./redis.conf
+for i in {1..$1}
+do
+    ./src/redis-server ./redis.conf --port $((6379 + $i))&
+done
