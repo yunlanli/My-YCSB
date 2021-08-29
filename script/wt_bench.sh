bb0b182b47c77b890ab5167a7844daad226adbcb
Yuhong Zhong <nanua.zqz@gmail.com>
2021-08-28 21:45:25 -0700

    improve wt_bench

--- script/wt_bench.sh
+++ script/wt_bench.sh
@@ -1,17 +1,22 @@
-for workload in "ycsb_a" "ycsb_b" "ycsb_c" "ycsb_d" "ycsb_e" "ycsb_f" "uniform"
+thread_count_list="1 2 3"
+cache_size_list="4G 2G 1G 512M"
+workload_list="ycsb_a ycsb_b ycsb_c ycsb_d ycsb_e ycsb_f uniform"
+
+for workload in ${workload_list}
 do
-    if [ "$workload" = "uniform" ]; then
+    if [ "$workload" = "uniform" ] || [ "$workload" = "ycsb_c" ]; then
         ./reset_db.sh
     fi
+
     # run baseline
     ~/disable_wt_ebpf.sh
-    for thread_count in 1 2 3
+    for thread_count in ${thread_count_list}
     do
         ./change_thread_count.sh ${thread_count}
-        for cache_size in 8G 4G 2G 1G 512M 256M 128M 64M 32M
+        for cache_size in ${cache_size_list}
         do
             ./change_cache_size.sh ${cache_size}
-            if [ "$workload" != "uniform" ]; then
+            if [ "$workload" != "uniform" ] && [ "$workload" != "ycsb_c" ]; then
                 ./reset_db.sh
             fi
             ./run_wt ../wiredtiger/config/${workload}.yaml | tee run_log/32G_${cache_size}_${thread_count}Thread_Unmodified_${workload^^}.txt
@@ -22,13 +27,13 @@ do
     ~/enable_wt_ebpf.sh
     ../../ebpf/tools/build/init_syscall
     sudo dmesg -C
-    for thread_count in 1 2 3
+    for thread_count in ${thread_count_list}
     do
         ./change_thread_count.sh ${thread_count}
-        for cache_size in 8G 4G 2G 1G 512M 256M 128M 64M 32M
+        for cache_size in ${cache_size_list}
         do
             ./change_cache_size.sh ${cache_size}
-            if [ "$workload" != "uniform" ]; then
+            if [ "$workload" != "uniform" ] && [ "$workload" != "ycsb_c" ]; then
                 ./reset_db.sh
             fi
             ./run_wt ../wiredtiger/config/${workload}.yaml | tee run_log/32G_${cache_size}_${thread_count}Thread_BPF_${workload^^}.txt
